695475833fd2fc15ad438ec3537ac3094db700dd
Fayeez Ahmed <fayeez@kairos-vision.com>
2013-10-20 00:36:25 +0100

    Mobile device checker

--- js/background-fader.js
+++ js/background-fader.js
@@ -19,8 +19,6 @@ var isMobile = {
     }
 };
 
-if( isMobile.any() ) alert('Mobile');
-
 function fader(fadeVal)
 {
     width = checkScreenSize();
@@ -49,24 +47,26 @@ function fader(fadeVal)
     }
     
     else {
-        var url= window.location.href;
-        var spliturl = url.split("/");
-        // Check to see if we are on the home page and only perform the background image styling
-        // on those since we won't see it on the other pages
-        if (spliturl.length < 5) {
-            images = new Array();
-            images = ["images/CreatingSwirls.jpg", "images/NasMatic-Final.jpg", "images/WorldWonders.jpg", "images/water_monk.jpg", "images/Kairos_Street.jpg", "images/UltimateGohanFinal.jpg"];
-            num = Math.floor(Math.random() * images.length) + 0;
-            imagepath = "http://www.kairos-vision.com/wp-content/themes/kairos-vision-website/";
-            width = checkScreenSize();
-            document.getElementById("static-bg").style.backgroundColor="black";
-            document.getElementById("static-bg").style.background="url('" + imagepath + images[num] + "')";
-            document.getElementById("static-bg").style.backgroundSize="100%";
-            document.getElementById("static-bg").style.backgroundRepeat="repeat";
-            document.getElementById("static-bg").style.backgroundAttachment="fixed";
-            document.getElementById("static-bg").style.backgroundPosition="center";
-            //document.getElementById("static-bg").style.marginTop="-250px";
-            //document.getElementById("static-bg").style.marginleft="-250px";
+        if( isMobile.any() ) {
+            var url= window.location.href;
+            var spliturl = url.split("/");
+            // Check to see if we are on the home page and only perform the background image styling
+            // on those since we won't see it on the other pages
+            if (spliturl.length < 5) {
+                images = new Array();
+                images = ["images/CreatingSwirls.jpg", "images/NasMatic-Final.jpg", "images/WorldWonders.jpg", "images/water_monk.jpg", "images/Kairos_Street.jpg", "images/UltimateGohanFinal.jpg"];
+                num = Math.floor(Math.random() * images.length) + 0;
+                imagepath = "http://www.kairos-vision.com/wp-content/themes/kairos-vision-website/";
+                width = checkScreenSize();
+                document.getElementById("static-bg").style.backgroundColor="black";
+                document.getElementById("static-bg").style.background="url('" + imagepath + images[num] + "')";
+                document.getElementById("static-bg").style.backgroundSize="100%";
+                document.getElementById("static-bg").style.backgroundRepeat="repeat";
+                document.getElementById("static-bg").style.backgroundAttachment="fixed";
+                document.getElementById("static-bg").style.backgroundPosition="center";
+                //document.getElementById("static-bg").style.marginTop="-250px";
+                //document.getElementById("static-bg").style.marginleft="-250px";
+            }
         }
     }
 }
