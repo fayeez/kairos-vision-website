e59de9de3418a4da34397d0da6e8f73c5989f65b
Fayeez Ahmed <fayeez@kairos-vision.com>
2013-10-20 00:39:58 +0100

    Mobile device checker

--- js/background-fader.js
+++ js/background-fader.js
@@ -46,27 +46,31 @@ function fader(fadeVal)
     }
     
     else {
-        var $j=jQuery.noConflict()
-        $j(".fader .img-bg").css('opacity', fadeVal);
-        $j(".fader #1").show("fade", 2000);
-        $j(".fader #1").delay(4000).hide("fade", 2000);
-
-        var src=$j(".fader .img-bg").size();
-        var count = 2;
-
-        setInterval(function(){
+        width = checkScreenSize();
+        if (width > 580){
+            var $j=jQuery.noConflict()
+            $j(".fader .img-bg").css('opacity', fadeVal);
+            $j(".fader #1").show("fade", 2000);
+            $j(".fader #1").delay(4000).hide("fade", 2000);
+    
+            var src=$j(".fader .img-bg").size();
+            var count = 2;
+    
+            setInterval(function(){
+                
+                $j(".fader #"+count).show("fade", 2000);
+                $j(".fader #"+count).delay(4000).hide("fade", 2000);
+                
+                if(count==src){
+                    count=1;
+                }
+                else{
+                    count=count+1;
+                }
+                
+                }, 7600)
             
-            $j(".fader #"+count).show("fade", 2000);
-            $j(".fader #"+count).delay(4000).hide("fade", 2000);
-            
-            if(count==src){
-                count=1;
-            }
-            else{
-                count=count+1;
-            }
-            
-            }, 7600)
+        }
         
         }
     }
