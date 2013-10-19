02fbad24223abb3ed18b5c4733c10e0759ccde80
Fayeez Ahmed <fayeez@kairos-vision.com>
2013-10-19 22:52:00 +0100

    Modified background fader to have else statement

--- js/background-fader.js
+++ js/background-fader.js
@@ -32,8 +32,9 @@ function fader(fadeVal)
         imagepath = "http://www.kairos-vision.com/wp-content/themes/kairos-vision-website/";
         width = checkScreenSize();
         document.getElementById("static-bg").style.background="url('" + imagepath + images[num] + "')";
-        document.getElementById("static-bg").style.backgroundSize="100%";
+        //document.getElementById("static-bg").style.backgroundSize="100%";
         document.getElementById("static-bg").style.backgroundRepeat="no-repeat";
+        document.getElementById("static-bg").style.backgroundAttachment="fixed";
         document.getElementById("static-bg").style.backgroundPosition="center";
         //document.getElementById("static-bg").style.marginTop="-250px";
         //document.getElementById("static-bg").style.marginleft="-250px";
