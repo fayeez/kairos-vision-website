7dfeac2831be647a571fcebda84a5d36799305e3
Fayeez Ahmed <fayeez@kairos-vision.com>
2013-10-19 20:54:14 +0100

    Only apply background fader when screen size is greater than 580

--- js/background-fader.js
+++ js/background-fader.js
@@ -1,24 +1,27 @@
 function fader(fadeVal)
 {
-var $j=jQuery.noConflict()
-$j(".fader .img-bg").css('opacity', fadeVal);
-$j(".fader #1").show("fade", 2000);
-$j(".fader #1").delay(4000).hide("fade", 2000);
+    width = checkScreenSize();
+    if(width > 580) {
+        var $j=jQuery.noConflict()
+        $j(".fader .img-bg").css('opacity', fadeVal);
+        $j(".fader #1").show("fade", 2000);
+        $j(".fader #1").delay(4000).hide("fade", 2000);
 
-var src=$j(".fader .img-bg").size();
-var count = 2;
+        var src=$j(".fader .img-bg").size();
+        var count = 2;
 
-setInterval(function(){
-    
-    $j(".fader #"+count).show("fade", 2000);
-    $j(".fader #"+count).delay(4000).hide("fade", 2000);
-    
-    if(count==src){
-        count=1;
+        setInterval(function(){
+            
+            $j(".fader #"+count).show("fade", 2000);
+            $j(".fader #"+count).delay(4000).hide("fade", 2000);
+            
+            if(count==src){
+                count=1;
+            }
+            else{
+                count=count+1;
+            }
+            
+            }, 7600)
     }
-    else{
-        count=count+1;
-    }
-    
-    }, 7600)
 }
