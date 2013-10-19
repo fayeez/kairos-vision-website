24325bc705c3b868f0053a2d9f9556409bb7e142
Fayeez Ahmed <fayeez@kairos-vision.com>
2013-10-19 23:24:41 +0100

    Modified background fader to have else statement

--- js/background-fader.js
+++ js/background-fader.js
@@ -28,20 +28,20 @@ function fader(fadeVal)
     else {
         var url= window.location.href;
         var spliturl = url.split("/");
-        alert (spliturl.length);
-        images = new Array();
-        images = ["images/CreatingSwirls.jpg", "images/NasMatic-Final.jpg", "images/WorldWonders.jpg", "images/water_monk.jpg", "images/Kairos_Street.jpg", "images/UltimateGohanFinal.jpg"];
-        num = Math.floor(Math.random() * images.length) + 0;
-        imagepath = "http://www.kairos-vision.com/wp-content/themes/kairos-vision-website/";
-        width = checkScreenSize();
-        document.getElementById("static-bg").style.backgroundColor="black";
-        document.getElementById("static-bg").style.background="url('" + imagepath + images[num] + "')";
-        document.getElementById("static-bg").style.backgroundSize="100%";
-        document.getElementById("static-bg").style.backgroundRepeat="repeat";
-        document.getElementById("static-bg").style.backgroundAttachment="fixed";
-        document.getElementById("static-bg").style.backgroundPosition="center";
-        //document.getElementById("static-bg").style.marginTop="-250px";
-        //document.getElementById("static-bg").style.marginleft="-250px";
-        
+        if (spliturl < 5) {
+            images = new Array();
+            images = ["images/CreatingSwirls.jpg", "images/NasMatic-Final.jpg", "images/WorldWonders.jpg", "images/water_monk.jpg", "images/Kairos_Street.jpg", "images/UltimateGohanFinal.jpg"];
+            num = Math.floor(Math.random() * images.length) + 0;
+            imagepath = "http://www.kairos-vision.com/wp-content/themes/kairos-vision-website/";
+            width = checkScreenSize();
+            document.getElementById("static-bg").style.backgroundColor="black";
+            document.getElementById("static-bg").style.background="url('" + imagepath + images[num] + "')";
+            document.getElementById("static-bg").style.backgroundSize="100%";
+            document.getElementById("static-bg").style.backgroundRepeat="repeat";
+            document.getElementById("static-bg").style.backgroundAttachment="fixed";
+            document.getElementById("static-bg").style.backgroundPosition="center";
+            //document.getElementById("static-bg").style.marginTop="-250px";
+            //document.getElementById("static-bg").style.marginleft="-250px";
+        }
     }
 }
