a9298307c114fa1163e4da5936b525fae99ed0ab
Fayeez Ahmed <fayeez@kairos-vision.com>
2013-11-30 11:57:10 +0000

    Unified custom javascripts to one kairosJavascripts.js file for speed.

--- js/scriptLoader.js
+++ js/scriptLoader.js
@@ -1,8 +1,6 @@
 var jquery = 'https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js';
 var jqueryUI = 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.0/jquery-ui.min.js';
-var backgroundFader = templateDir + '/js/background-fader.js';
-var dynamicFlow = templateDir + '/js/dynamic-flow.js';
-var checkSS = templateDir + '/js/check-screen-size.js';
+var backgroundFader = templateDir + '/js/kairosJavascript.js';
 
 var scriptUrls = [jquery, jqueryUI, backgroundFader, dynamicFlow, checkSS];
 
