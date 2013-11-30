7809d8f554ed67eebcd7f035155be71e7ecc5bb0
Fayeez Ahmed <fayeez@kairos-vision.com>
2013-11-30 11:52:50 +0000

    Epxpanded background-fader and dynamic-flow into separate javascript. Also removed seemingly redundant check screen size js

--- js/scriptLoader.js
+++ js/scriptLoader.js
@@ -1,7 +1,7 @@
 var jquery = 'https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js';
 var jqueryUI = 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.0/jquery-ui.min.js';
-var backgroundFader = templateDir + '/js/kairosJavascripts.js';
-//var dynamicFlow = templateDir + '/js/dynamic-flow.js';
+var backgroundFader = templateDir + '/js/background-fader.js';
+var dynamicFlow = templateDir + '/js/dynamic-flow.js';
 //var checkSS = templateDir + '/js/check-screen-size.js';
 
 var scriptUrls = [jquery, jqueryUI, backgroundFader, dynamicFlow, checkSS];
