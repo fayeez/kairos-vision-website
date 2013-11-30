04db385dbc956d7fb9d96084f3f8fef4e21b55d1
Fayeez Ahmed <fayeez@kairos-vision.com>
2013-11-30 11:53:57 +0000

    Added check screen size javascript back in

--- js/scriptLoader.js
+++ js/scriptLoader.js
@@ -2,7 +2,7 @@ var jquery = 'https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js';
 var jqueryUI = 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.0/jquery-ui.min.js';
 var backgroundFader = templateDir + '/js/background-fader.js';
 var dynamicFlow = templateDir + '/js/dynamic-flow.js';
-//var checkSS = templateDir + '/js/check-screen-size.js';
+var checkSS = templateDir + '/js/check-screen-size.js';
 
 var scriptUrls = [jquery, jqueryUI, backgroundFader, dynamicFlow, checkSS];
 
