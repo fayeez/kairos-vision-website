b1b3d0a3f7deecce9b42423ed3719b26e0ffa0b4
Fayeez Ahmed <fayeez@kairos-vision.com>
2013-11-30 11:58:02 +0000

    Corrected typo in file

--- js/scriptLoader.js
+++ js/scriptLoader.js
@@ -1,6 +1,6 @@
 var jquery = 'https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js';
 var jqueryUI = 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.0/jquery-ui.min.js';
-var backgroundFader = templateDir + '/js/kairosJavascript.js';
+var backgroundFader = templateDir + '/js/kairosJavascripts.js';
 
 var scriptUrls = [jquery, jqueryUI, backgroundFader, dynamicFlow, checkSS];
 
