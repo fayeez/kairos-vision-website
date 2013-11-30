ef6da67cfc14697b210f6542308f3cdd24ddb9e6
Fayeez Ahmed <fayeez@kairos-vision.com>
2013-11-30 11:40:44 +0000

    Modified loading of javascripts to attempt to make it load faster

--- jquery.php
+++ jquery.php
@@ -1,17 +1,20 @@
-<?php function myJqueryScripts(){
+<?php function jQueryScripts(){
     
-    wp_register_script("jquery","https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js", false);
-    wp_register_script('jquery-ui', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.0/jquery-ui.min.js', array('jquery'), '1.10.0');
-    wp_enqueue_script("jquery");
-    wp_enqueue_script("jquery-ui");
+    //wp_register_script("jquery","https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js", false);
+    //wp_register_script('jquery-ui', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.0/jquery-ui.min.js', array('jquery'), '1.10.0');
+    //wp_enqueue_script("jquery");
+    //wp_enqueue_script("jquery-ui");
+    //
+    //wp_register_script("background-fader",get_bloginfo('template_directory').'/js/background-fader.js', array('jquery'));
+    //wp_enqueue_script('background-fader');
+    //
+    //wp_register_script('dynamic-flow', get_bloginfo('template_directory').'/js/dynamic-flow.js', array('jquery'));
+    //wp_enqueue_script('dynamic-flow');
+    //
+    //wp_register_script('check-screen-size', get_bloginfo('template_directory').'/js/check-screen-size.js', array('jquery'));
+    //wp_enqueue_script('check-screen-size');
     
-    wp_register_script("background-fader",get_bloginfo('template_directory').'/js/background-fader.js', array('jquery'));
-    wp_enqueue_script('background-fader');
-    
-    wp_register_script('dynamic-flow', get_bloginfo('template_directory').'/js/dynamic-flow.js', array('jquery'));
-    wp_enqueue_script('dynamic-flow');
-
-    wp_register_script('check-screen-size', get_bloginfo('template_directory').'/js/check-screen-size.js', array('jquery'));
-    wp_enqueue_script('check-screen-size');
+    wp_register_script("jquery-script-loader", get_bloginfo('template_directory').'/js/scriptLoader.js', false);
+    wp_enqueue_script("jquery-script-loader");
 }
-add_action('wp_print_scripts','myJqueryScripts'); ?>
+add_action('wp_print_scripts','jQueryScripts'); ?>
---
+++ js/scriptLoader.js
@@ -0,0 +1,14 @@
+var templateDir = "<?php bloginfo('template_directory') ?>";
+var jquery = 'https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js';
+var jqueryUI = 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.0/jquery-ui.min.js';
+var backgroundFader = templateDir + '/js/background-fader.js';
+var dynamicFlow = templateDir + '/js/dynamic-flow.js';
+var checkSS = templateDir + '/js/check-screen-size.js';
+
+var scriptUrls = [jquery, jqueryUI, backgroundFader, dynamicFlow, checkSS];
+
+scriptUrls.forEach(function(url,index,scriptUrls){
+  var script = document.createElement('script');
+  document.getElementsByTagName('head')[0].appendChild(script);
+  script.src = url;
+});
