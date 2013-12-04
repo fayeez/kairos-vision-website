ba0b1a56e032f259058d8e5e56715897aea6c41d
Fayeez Ahmed <fayeez@kairos-vision.com>
2013-12-04 14:43:29 +0000

    Added post caption to portfolio images

--- new-portfolio.php
+++ new-portfolio.php
@@ -46,7 +46,7 @@ Template Name: Client Portfolio - 2 Columns
 											<h4><?php echo $attachment->post_title; ?></h4>
 											<?php
 											echo $attachment->post_content;?>
-											<a href"<?php $attachment->post_excerpt;?>"> Website </a>
+											<a href="<?php $attachment->post_excerpt;?>">. Website </a>
 										</div>
 
 								<?php	}
