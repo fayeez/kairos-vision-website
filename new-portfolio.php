fce3dc7171e61996dd89adf30622dc8c47ce2e86
Fayeez Ahmed <fayeez@kairos-vision.com>
2014-01-05 19:54:22 +0000

    Added  strong tags around the title of each client work

--- new-portfolio.php
+++ new-portfolio.php
@@ -43,7 +43,7 @@ Template Name: Client Portfolio - 2 Columns
 										</a>
 										</div>
 										<div class="box20 portfolio-description">
-											<h4><strong><?php echo $attachment->post_title; ?></strong></h4>
+											<strong><h4><?php echo $attachment->post_title; ?></h4></strong>
 											<?php
 											echo $attachment->post_content;
 											if ($attachment->post_excerpt){
