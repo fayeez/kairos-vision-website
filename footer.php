7938fda54aa4a2f8837324ea66af039dec4fb9b9
Fayeez Ahmed <fayeez@kairos-vision.com>
2014-01-24 19:37:48 +0000

    Added twitter and facebook event clicks

--- footer.php
+++ footer.php
@@ -13,14 +13,14 @@
                     <ul class="footer-social" id="footer-social-pos">
                         <li>
                             <?php if(isset($options['fburl'])){ ?>
-                                <a href="https://www.facebook.com/kairosvis"> <img src="<?php bloginfo('template_directory'); ?>/images/icons/facebook-icon-col.png" class="social-icon-size" alt="Facebook Profile <?php echo $options['fburl'] ?>"/></a> 
+                                <a href="https://www.facebook.com/kairosvis" onclick="(['_trackEvent', 'Facebook', 'Went to Facebook Page', 'Social']);"> <img src="<?php bloginfo('template_directory'); ?>/images/icons/facebook-icon-col.png" class="social-icon-size" alt="Facebook Profile <?php echo $options['fburl'] ?>"/></a> 
                             <?php }
                             else { ?>
-                                <li><a href="https://www.facebook.com/kairosvis"> <img src="<?php bloginfo('template_directory'); ?>/images/icons/facebook-icon-col.png" class="social-icon-size" alt="icon-facebok"/></a></li>
+                                <li><a href="https://www.facebook.com/kairosvis" onclick="(['_trackEvent', 'Facebook', 'Went to Facebook Page', 'Social']);"> <img src="<?php bloginfo('template_directory'); ?>/images/icons/facebook-icon-col.png" class="social-icon-size" alt="icon-facebok"/></a></li>
                             <?php } ?>
                             
                         </li>
-                        <li><a href="https://twitter.com/kairosvi"> <img src="<?php bloginfo('template_directory'); ?>/images/icons/twitter-icon-col.png" class="social-icon-size" alt="icon-twitter"/></a></li>
+                        <li><a href="https://twitter.com/kairosvi" onclick="(['_trackEvent', 'Twitter', 'Went to Twitter Page', 'Social']);"> <img src="<?php bloginfo('template_directory'); ?>/images/icons/twitter-icon-col.png" class="social-icon-size" alt="icon-twitter"/></a></li>
                         <li><a href="http://www.linkedin.com"> <img src="<?php bloginfo('template_directory'); ?>/images/icons/linkedin-icon-col.png" class="social-icon-size" alt="icon-linkedin"/></a></li>
                         <li><a href="#"> <img src="<?php bloginfo('template_directory'); ?>/images/icons/rss-icon-col.png" class="social-icon-size" alt="icon-rss"/></a></li>
                     </ul>
@@ -30,3 +30,4 @@
         
     </div>
 </footer>
+
